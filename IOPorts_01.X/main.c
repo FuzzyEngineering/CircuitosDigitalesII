026c2f45a3ab88c72449dc1ed3004398a4fcece8
GEHRSE <sebastian.puente@unillanos.edu.co>
2018-03-18 02:19:30 -0500

    ajuste LATD
    inicializar el puerto D

--- IOPorts_01.X/main.c
+++ IOPorts_01.X/main.c
@@ -5,13 +5,13 @@
  * Processor:       PIC18F45K20
  * Compiler:        XC8
  * Version:         1.45
- * Author:          Sebastián Fernando Puente Reyes
+ * Author:          Sebastiï¿½n Fernando Puente Reyes
  * e-mail:          sebastian.puente@unillanos.edu.co
  * Date:            Marzo de 2018
  *******************************************************************************
- * DESCRIPCIÓN
+ * DESCRIPCIï¿½N
  * Oscilador: Interno a 16 MHz, Fosc = 16 MHz.
- * Requerimiento: Encender un LED conectado en la línea RD0.
+ * Requerimiento: Encender un LED conectado en la lï¿½nea RD0.
  ******************************************************************************/
 /*******************************************************************************
  * Librerias
@@ -32,29 +32,30 @@
  ******************************************************************************/
 
 /*******************************************************************************
- * Función Principal
+ * Funciï¿½n Principal
  ******************************************************************************/
 void main(void)
 {
-    //Configuración frecuencia oscilador interno
+    //Configuraciï¿½n frecuencia oscilador interno
     OSCCONbits.IRCF = 0b111;    //Oscilador interno a 16 MHz, Fosc = 16Mhz
     
-    //Configuración puertos I/O
-    TRISDbits.RD0 = 0;          //Línea RD0 como salida digital
-    
+    //Configuraciï¿½n puertos I/O
+    LATD = 0;
+    TRISDbits.RD0 = 0;          //Lï¿½nea RD0 como salida digital
+
     while(1)
     {
-        //Encender LED conectado en la línea RD0
-        PORTDbits.RD0 = 1;  //Nivel alto por la línea RD0
+        //Encender LED conectado en la lï¿½nea RD0
+        PORTDbits.RD0 = 1;  //Nivel alto por la lï¿½nea RD0
     }
 }
 
 /*******************************************************************************
- * DISEÑO DIGITAL CON MICROCONTROLADORES PIC DE 8 BITS
- * Sebastián Puente Reyes, M.Sc.
+ * DISEï¿½O DIGITAL CON MICROCONTROLADORES PIC DE 8 BITS
+ * Sebastiï¿½n Puente Reyes, M.Sc.
  * Grupo de Estudio en Hardware Reconfigurable y Sistemas Embebidos - GEHRSE
- * Escuela de Ingeniería
- * Facultad de Ciencías Básicas e Ingeniería
+ * Escuela de Ingenierï¿½a
+ * Facultad de Ciencï¿½as Bï¿½sicas e Ingenierï¿½a
  * Universidad de los Llanos
  * Villavicencio - Meta, Colombia
  ******************************************************************************/
